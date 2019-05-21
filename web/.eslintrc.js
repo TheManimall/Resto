17793a2d43e2a0ea4a547ed4e48e0d9e8183e9ee
TheManimall <yuriyvovk5@gmail.com>
2019-05-21 14:23:36 +0300

    sign in && sign up

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,23 +1,24 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
   extends: 'airbnb',
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react'],
+  extends: 'react-app',
+  plugins: ['prettier'],
   rules: {
-  },
+    'prettier/prettier': 'error'
+  }
 };
